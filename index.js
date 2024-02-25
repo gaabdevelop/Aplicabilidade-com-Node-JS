ee1f4b72cb1ae7605b505ef48e7d1bf9f9838fb4
Gabriel de Carli <154674155+gaabdevelop@users.noreply.github.com>
2024-02-25 18:36:36 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const puppeteer = require("puppeteer");
+
+const URL = https://www.google.com/search?q=previs%C3%A3o+do+tempo&rlz=1C1CHBD_pt-PTBR1092BR1092&oq=previs%C3%A3o&gs_lcrp=EgZjaHJvbWUqEQgAEEUYJxg7GJ0CGIAEGIoFMhEIABBFGCcYOxidAhiABBiKBTIOCAEQRRgnGDsYgAQYigUyBggCEEUYOzIGCAMQRRg5Mg0IBBAAGJIDGIAEGIoFMg0IBRAAGJIDGIAEGIoFMgYIBhBFGD0yBggHEEUYPKgCALACAA&sourceid=chrome&ie=UTF-8;
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(URL, { waitUntil: "networkidle2" });
+
+  await page.screenshot({ path: "./print.jpeg" });
+
+  let data = await page.evaluate(() => {
+    let temperatura = document.querySelector(".wob_t.q8U8x").innerHTML;
+    let chuva = document.querySelector("#wob_pp").innerHTML;
+    let umidade = document.querySelector("#wob_hm").innerHTML;
+    let vento = document.querySelector("#wob_ws").innerHTML;
+    return {
+      temperatura,
+      chuva,
+      umidade,
+      vento,
+    };
+  });
+
+  console.log("Temperatura: " + data.temperatura + "°C");
+  console.log("Chance de Chover: " + data.chuva);
+  console.log("Umidade: " + data.umidade);
+  console.log("Velocidade do Vento: " + data.vento);
+
+  await browser.close();
+})();
